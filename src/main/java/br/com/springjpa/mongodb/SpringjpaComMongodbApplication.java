ee98c9074b004ef8caaf85d54f534c750754c6c0
mdomingues53 <mdomingues53@hotmail.com>
2024-06-05 19:00:54 -0300

    first commit

---
+++ src/main/java/br/com/springjpa/mongodb/SpringjpaComMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.springjpa.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpaComMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpaComMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springjpa/mongodb/SpringjpaComMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springjpa.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpaComMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
